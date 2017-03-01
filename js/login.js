3827bb4d3eaa867b73a047a911952c2733f76b65
tomiamao <tomiground@hotmail.com>
2017-03-01 10:48:42 +0100

    initial commit

---
+++ js/login.js
@@ -0,0 +1,15 @@
+var app = angular.module('LoginForm',['ngMaterial','ngAnimate','ngAria','ngMessages'])
+.controller('Ctrl', function($scope){
+	/*
+  $scope.vm = {
+      formData: {
+        email: 'hello@photoapp.com',
+       	password: 'password'
+      }
+  };*/
+
+  $scope.login = function() {
+  	
+  }
+  
+});
